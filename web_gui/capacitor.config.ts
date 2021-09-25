e6a59212878bf66735a8145dd51a04deaaaf8d8f
Liam Bradford <liamjordanconsultancy@gmail.com>
2021-09-26 05:34:28 +1000

    capacitor android up and running. I think web_gui/dist is being tracked though along with android build artifacts for capactior

---
+++ web_gui/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.heliam1.gui_benchmark',
+  appName: 'web_gui',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
